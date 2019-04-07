2c49b79297c889327f7349d1c79fa7b0f525650b
Sangram <reddy.horcrux@gmail.com>
2019-04-07 15:19:57 +0100

    Corrected initial migrations

--- src/migrations/customer/00002_initial_seed.sql
+++ src/migrations/customer/00002_initial_seed.sql
@@ -1,8 +1,8 @@
 -- Payment types
-INSERT INTO payment_type VALUES (1, 'Debit Card');
-INSERT INTO payment_type VALUES (2, 'Credit Card');
+INSERT INTO customer.payment_type VALUES (1, 'Debit Card');
+INSERT INTO customer.payment_type VALUES (2, 'Credit Card');
 
 -- Address types
-INSERT INTO address_type VALUES (1, 'Company');
-INSERT INTO address_type VALUES (2, 'Billing');
-INSERT INTO address_type VALUES (3, 'Delivery');
+INSERT INTO customer.address_type VALUES (1, 'Company');
+INSERT INTO customer.address_type VALUES (2, 'Billing');
+INSERT INTO customer.address_type VALUES (3, 'Delivery');
