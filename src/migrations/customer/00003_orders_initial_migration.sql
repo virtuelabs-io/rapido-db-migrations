bc116ccd403b9cfee3ca9c1958c5676f3557c376
Sangram Reddy <reddy.horcrux@gmail.com>
2019-07-14 15:25:29 +0100

    Added necessary product details stuff

--- src/migrations/customer/00003_orders_initial_migration.sql
+++ src/migrations/customer/00003_orders_initial_migration.sql
@@ -19,6 +19,26 @@ CREATE TABLE IF NOT EXISTS orders.vat (
     FOREIGN KEY (country_id) REFERENCES orders.country(id)
 );
 
+CREATE TABLE IF NOT EXISTS orders.products (
+    id MEDIUMINT,
+    unit_price DECIMAL(11,2),
+    quantity_in_stock DECIMAL(9,2),
+    active BOOLEAN DEFAULT TRUE,
+    created_on     DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_on DATETIME ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS orders.product_price_history (
+    id BIGINT AUTO_INCREMENT,
+    product_id MEDIUMINT,
+    unit_price DECIMAL(11,2),
+    created_on     DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_on DATETIME ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (product_id) REFERENCES orders.products(id),
+    PRIMARY KEY (id)
+);
+
 CREATE TABLE IF NOT EXISTS orders.header (
     id BIGINT AUTO_INCREMENT,
     customer_id BINARY(16),
@@ -40,5 +60,6 @@ CREATE TABLE IF NOT EXISTS orders.item (
     unit_price DECIMAL(11,2),
     quantity DECIMAL(9,2),
     PRIMARY KEY (id),
-    FOREIGN KEY (order_id) REFERENCES orders.header(id)
+    FOREIGN KEY (order_id) REFERENCES orders.header(id),
+    FOREIGN KEY (product_id) REFERENCES orders.products(id)
 );
--- src/migrations/customer/00102_orders_initial_seed.sql
+++ src/migrations/customer/00102_orders_initial_seed.sql
@@ -8,3 +8,14 @@ INSERT INTO orders.order_status VALUES (5, 'Returned');
 INSERT INTO orders.country VALUES (1, 'United Kingdom');
 -- Vat
 INSERT INTO orders.vat VALUES (1, 0.10);
+-- Products
+INSERT INTO orders.products (id, unit_price) VALUES (1, 222.99);
+INSERT INTO orders.products (id, unit_price) VALUES (2, 219.00);
+INSERT INTO orders.products (id, unit_price) VALUES (3, 229.17);
+INSERT INTO orders.products (id, unit_price) VALUES (4, 79.00);
+INSERT INTO orders.products (id, unit_price) VALUES (5, 49.00);
+INSERT INTO orders.products (id, unit_price) VALUES (6, 253.00);
+INSERT INTO orders.products (id, unit_price) VALUES (7, 49.00);
+INSERT INTO orders.products (id, unit_price) VALUES (8, 269.41);
+INSERT INTO orders.products (id, unit_price) VALUES (9, 49.99);
+INSERT INTO orders.products (id, unit_price) VALUES (10, 14.00);
