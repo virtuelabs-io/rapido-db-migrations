816d835afa10a27f37c562cac636ba183421a244
Sangram Reddy <reddy.horcrux@gmail.com>
2019-07-14 18:41:06 +0100

    Added not null

--- src/migrations/customer/00003_orders_initial_migration.sql
+++ src/migrations/customer/00003_orders_initial_migration.sql
@@ -41,7 +41,7 @@ CREATE TABLE IF NOT EXISTS orders.product_price_history (
 
 CREATE TABLE IF NOT EXISTS orders.header (
     id BIGINT AUTO_INCREMENT,
-    customer_id BINARY(16),
+    customer_id BINARY(16) NOT NULL,
     order_price DECIMAL(11,2),
     order_status_id TINYINT,
     charge_id VARCHAR(100),
@@ -55,7 +55,7 @@ CREATE TABLE IF NOT EXISTS orders.header (
 
 CREATE TABLE IF NOT EXISTS orders.item (
     id BIGINT AUTO_INCREMENT,
-    order_id BIGINT,
+    order_id BIGINT NOT NULL,
     product_id MEDIUMINT,
     unit_price DECIMAL(11,2),
     quantity DECIMAL(9,2),
